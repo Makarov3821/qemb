316cc70db51a32674c36e13775927757a4b4ceb9
Makarov3821 <ylzhao22@m.fudan.edu.cn>
2025-11-05 21:51:29 +0800

    Add licence files

--- src/qemb/_version.py
+++ src/qemb/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev0+d20251105'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20251105')
+__version__ = version = '0.1.1.dev0+g9e5112ac9.d20251105'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g9e5112ac9.d20251105')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'g9e5112ac9'
